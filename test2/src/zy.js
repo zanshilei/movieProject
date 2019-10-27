0360a9ab6356634d9ad5939880b1864329a509a9
陶懿臻 <wb-tyz279201@alibaba-inc.com>
2019-10-27 22:57:05 +0800

    test

--- test2/src/zy.js
+++ test2/src/zy.js
@@ -1,7 +1,7 @@
 "use strict";
 import React from "react"
 import ReactDOM from "react-dom";
-import {Router,Route,hashHistory,IndexRoute,Redirect} from "react-router";
+import { Router, Route, hashHistory, IndexRoute, Redirect } from "react-router";
 import Movie from './zy/movie';
 import Index from './zy/index';
 import Cinema from './zy/cinema';
@@ -10,33 +10,33 @@ import Recommend from './zy/recommend';
 import './styles/zy.scss';
 import TabBarActions from './fluxs/actions/tabBarActions';
 import TabBarStore from './fluxs/stores/tabBarStore'
-let onEnterHandler=(type)=>{
+let onEnterHandler = (type) => {
 	//let x=`abc`;
 	//console.log(type)
 	TabBarActions.setItem(type);
 	//console.log(`${x}`+type)
 }
-let onLiveHandler=(type)=>{
-	let x=`123`;
-	
+let onLiveHandler = (type) => {
+	let x = `123`;
+	console.log(10)
 	//console.log(TabBarActions.get)
 	//console.log(`${x}`)
 }
-ReactDOM.render((	
-	<Router history={hashHistory}>	          
-		<Route path='/' component={Index}>		
-			<IndexRoute component={Recommend}></IndexRoute>	
-			<Route path='recommend' onLeave={onLiveHandler} onEnter={onEnterHandler.bind(this,'recommend')} component={Recommend}>						    				    
+ReactDOM.render((
+	<Router history={hashHistory}>
+		<Route path='/' component={Index}>
+			<IndexRoute component={Recommend}></IndexRoute>
+			<Route path='recommend' onLeave={onLiveHandler} onEnter={onEnterHandler.bind(this, 'recommend')} component={Recommend}>
 			</Route>
-			<Route path='movie' component={Movie} onEnter={onEnterHandler.bind(this,'movie')}>	
+			<Route path='movie' component={Movie} onEnter={onEnterHandler.bind(this, 'movie')}>
 			</Route>
-			<Route path='mine' component={Mine} onEnter={onEnterHandler.bind(this,'mine')}>
+			<Route path='mine' component={Mine} onEnter={onEnterHandler.bind(this, 'mine')}>
 			</Route>
-			<Route path='cinema1234' component={Cinema} onEnter={onEnterHandler.bind(this,'cinema')}>
-			</Route>	
-		</Route>		
+			<Route path='cinema1234' component={Cinema} onEnter={onEnterHandler.bind(this, 'cinema')}>
+			</Route>
+		</Route>
 	</Router>
-	),
+),
 	document.getElementById("app")
 )
 
